1681262f928b20019e5de6e1e5ac561933532298
franipendus <147450776+franipendus@users.noreply.github.com>
2024-05-30 20:51:55 +0200

    Update databaseInit.sql

--- databaseInit.sql
+++ databaseInit.sql
@@ -262,6 +262,7 @@ CREATE TABLE IF NOT EXISTS advertisers
     id         INTEGER UNIQUE      NOT NULL,
     username   VARCHAR(255) UNIQUE NOT NULL,
     password   VARCHAR(255)        NOT NULL,
+    company    VARCHAR(255)        NOT NULL,
     first_name VARCHAR(255)        NOT NULL,
     last_name  VARCHAR(255)        NOT NULL,
     ads_posted INTEGER             NOT NULL,
